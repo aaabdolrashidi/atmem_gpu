ac21bff1e04c1c96ccc971adcff55fda2f62cc60
Abdolrashidi <aabdo001@ucr.edu>
2018-10-10 17:18:15 -0700

    The program will print if a cuda error occurs during the elapsed_time memory transfer. Also, Added more security measures for the benchmarks.

--- atmem_benchmark/kernel.cu
+++ atmem_benchmark/kernel.cu
@@ -1,12 +1,12 @@
-typedef unsigned long long TimeType;
+typedef signed long long TimeType;
 /******************************************************************************
 * Host Functions
 *******************************************************************************/
 
 TimeType find_max(TimeType* time_array, int length)
 {
-	TimeType result = 0;
-	for (int i = 0; i < length; i++)
+	TimeType result = time_array[0];
+	for (int i = 1; i < length; i++)
 	{
 		if (result < time_array[i]) result = time_array[i];
 	}
@@ -18,32 +18,44 @@ TimeType find_max(TimeType* time_array, int length)
 *******************************************************************************/
 __global__ void lmwTest_baseline(float* data, float scalar, int blockSize, TimeType* elapsed_time) {
 	TimeType start_time, end_time, temp;
-	start_time = clock64();
+	if (threadIdx.x == 0) start_time = clock64();
+	__syncthreads();
 	// Begin	
 	int index = (blockIdx.x*blockDim.x + threadIdx.x)*blockSize;
 	for (int i = 0; i<blockSize; i++) {
 		data[index + i] += scalar;
 	}
 	// End
-	end_time = clock64();
-	temp = end_time - start_time;
-	elapsed_time[blockIdx.x] = temp;
-	// printf("Elapsed time: %u\n", temp);
+	__syncthreads();
+	if (threadIdx.x == 0)
+	{
+		end_time = clock64();
+		temp = end_time - start_time;
+		elapsed_time[blockIdx.x] = temp;
+		// printf("Elapsed time: %u\n", temp);
+	}
+	__threadfence();
 }
 
 __global__ void lmwTest_atomic(float* data, float scalar, int blockSize, TimeType* elapsed_time) {
 	TimeType start_time, end_time, temp;
-	start_time = clock64();
+	if (threadIdx.x == 0) start_time = clock64();
+	__syncthreads();
 	// Begin	
 	int index = (blockIdx.x*blockDim.x + threadIdx.x)*blockSize;
 	for (int i = 0; i<blockSize; i++) {
 		atomicAdd(&(data[index + i]), scalar);
 	}
 	// End
-	end_time = clock64();
-	temp = end_time - start_time;
-	elapsed_time[blockIdx.x] = temp;
-	// printf("Elapsed time: %u\n", temp);
+	__syncthreads();
+	if (threadIdx.x == 0)
+	{
+		end_time = clock64();
+		temp = end_time - start_time;
+		elapsed_time[blockIdx.x] = temp;
+		printf("Elapsed time: %lld\n", temp);
+	}
+	__threadfence();
 }
 
 /******************************************************************************
@@ -56,16 +68,17 @@ void atmem_bench(float* input, unsigned int num_elements, unsigned int block_siz
 	// Setting up time parameters
 	TimeType* elapsed_time_d;
 	TimeType* elapsed_time_h;
-	elapsed_time_h = (TimeType*)malloc(num_blocks * sizeof(TimeType));
-	for (int i = 0; i < num_blocks; i++)
-	{
-		elapsed_time_h[i] = 0;
-	}
+	int elapsed_time_size = num_blocks;
+	cudaError cuda_status;
 
-	cudaMalloc((void**)&elapsed_time_d, num_blocks * sizeof(TimeType));
+	elapsed_time_h = (TimeType*)malloc(elapsed_time_size * sizeof(TimeType));
+	for (int i = 0; i < elapsed_time_size; i++)
+		elapsed_time_h[i] = 3;	// Non-zero random value
+
+	cudaMalloc((void**)&elapsed_time_d, elapsed_time_size * sizeof(TimeType));
+	cudaMemset(elapsed_time_d, 0, elapsed_time_size * sizeof(TimeType));
 	cudaDeviceSynchronize();
-	cudaMemcpy(elapsed_time_d, elapsed_time_h, num_blocks * sizeof(TimeType), cudaMemcpyDeviceToHost);
-	cudaDeviceSynchronize();
+
 	// Events
 	cudaEvent_t start, stop;
 	cudaEventCreate(&start);
@@ -77,19 +90,28 @@ void atmem_bench(float* input, unsigned int num_elements, unsigned int block_siz
 		lmwTest_baseline << < num_blocks, block_size >> > (input, 1.0, block_size, elapsed_time_d);
 	else if (mode == 1)
 		lmwTest_atomic << < num_blocks, block_size >> > (input, 1.0, block_size, elapsed_time_d);
+	cudaDeviceSynchronize();
 	cudaEventRecord(stop);
 
 	cudaEventSynchronize(stop);
+
 	float total_elapsed_time = 0;
 	cudaEventElapsedTime(&total_elapsed_time, start, stop);
 
 	cudaDeviceSynchronize();
 
 	// Copying time to host
-	cudaMemcpy(elapsed_time_h, elapsed_time_d, num_blocks * sizeof(TimeType), cudaMemcpyDeviceToHost);
+	cuda_status = cudaMemcpy(elapsed_time_h, elapsed_time_d, elapsed_time_size * sizeof(TimeType), cudaMemcpyDeviceToHost);
+	if (cuda_status != cudaSuccess)
+	{
+		printf("\n*CUDA Error %d during cudaMemcpy(D -> H)!*\n\n", cuda_status);
+	}
 	cudaDeviceSynchronize();
+	//for (int i = 0; i < num_blocks; i++)
+	//	printf("%d\n", elapsed_time_h[i]);
+
 	printf("Total elapsed kernel time %f ms\n", total_elapsed_time);
-	printf("Max in-SM cycles: %u cycles\n", find_max(elapsed_time_h, num_blocks));
+	printf("Max in-SM cycles: %d cycles\n", find_max(elapsed_time_h, elapsed_time_size));
 
 	free(elapsed_time_h);
 	cudaFree(elapsed_time_d);
